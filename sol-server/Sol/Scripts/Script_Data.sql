0324eb6b82ed8020416cb201210676e17fe8fa7b
Zenon Trujillo <zenon.t@outlook.com>
2017-01-18 00:48:29 -0800

    data load

---
+++ sol-server/Sol/Scripts/Script_Data.sql
@@ -0,0 +1,18 @@
+﻿SET IDENTITY_INSERT [dbo].[DeliveryPoint] ON
+INSERT INTO [dbo].[DeliveryPoint] ([Id], [Name], [CreateUtc], [ModifyUtc]) VALUES (1, N'DELIVERY1', N'2017-01-18 08:45:23', NULL)
+INSERT INTO [dbo].[DeliveryPoint] ([Id], [Name], [CreateUtc], [ModifyUtc]) VALUES (2, N'DELIVERY2', N'2017-01-18 08:45:27', NULL)
+INSERT INTO [dbo].[DeliveryPoint] ([Id], [Name], [CreateUtc], [ModifyUtc]) VALUES (3, N'DELIVERY3', N'2017-01-18 08:45:29', NULL)
+INSERT INTO [dbo].[DeliveryPoint] ([Id], [Name], [CreateUtc], [ModifyUtc]) VALUES (4, N'DELIVERY4', N'2017-01-18 08:45:32', NULL)
+INSERT INTO [dbo].[DeliveryPoint] ([Id], [Name], [CreateUtc], [ModifyUtc]) VALUES (5, N'DELIVERY5', N'2017-01-18 08:45:35', NULL)
+INSERT INTO [dbo].[DeliveryPoint] ([Id], [Name], [CreateUtc], [ModifyUtc]) VALUES (6, N'DELIVERY6', N'2017-01-18 08:45:38', NULL)
+SET IDENTITY_INSERT [dbo].[DeliveryPoint] OFF
+
+SET IDENTITY_INSERT [dbo].[Market] ON
+INSERT INTO [dbo].[Market] ([Id], [Name], [CreateUtc], [ModifyUtc]) VALUES (1, N'CAL', N'2017-01-18 08:47:20', NULL)
+INSERT INTO [dbo].[Market] ([Id], [Name], [CreateUtc], [ModifyUtc]) VALUES (2, N'ERTC', N'2017-01-18 08:47:26', NULL)
+INSERT INTO [dbo].[Market] ([Id], [Name], [CreateUtc], [ModifyUtc]) VALUES (3, N'NYE', N'2017-01-18 08:47:37', NULL)
+INSERT INTO [dbo].[Market] ([Id], [Name], [CreateUtc], [ModifyUtc]) VALUES (4, N'NYW', N'2017-01-18 08:47:38', NULL)
+INSERT INTO [dbo].[Market] ([Id], [Name], [CreateUtc], [ModifyUtc]) VALUES (5, N'PJMW', N'2017-01-18 08:47:47', NULL)
+INSERT INTO [dbo].[Market] ([Id], [Name], [CreateUtc], [ModifyUtc]) VALUES (6, N'PJME', N'2017-01-18 08:47:50', NULL)
+SET IDENTITY_INSERT [dbo].[Market] OFF
+
