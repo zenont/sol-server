1cdf7d844021fec346487ab18415ef9b028a22a1
Zenon Trujillo <zenon.t@outlook.com>
2017-01-18 00:52:36 -0800

    zone data

--- sol-server/Sol/Scripts/Script_Data.sql
+++ sol-server/Sol/Scripts/Script_Data.sql
@@ -16,3 +16,12 @@ INSERT INTO [dbo].[Market] ([Id], [Name], [CreateUtc], [ModifyUtc]) VALUES (5, N
 INSERT INTO [dbo].[Market] ([Id], [Name], [CreateUtc], [ModifyUtc]) VALUES (6, N'PJME', N'2017-01-18 08:47:50', NULL)
 SET IDENTITY_INSERT [dbo].[Market] OFF
 
+SET IDENTITY_INSERT [dbo].[Zone] ON
+INSERT INTO [dbo].[Zone] ([Id], [MarketId], [Name], [CreateUtc], [ModifyUtc]) VALUES (1, 1, N'CAL_ZE', N'2017-01-18 08:49:11', NULL)
+INSERT INTO [dbo].[Zone] ([Id], [MarketId], [Name], [CreateUtc], [ModifyUtc]) VALUES (2, 1, N'CAL_ZW', N'2017-01-18 08:49:20', NULL)
+INSERT INTO [dbo].[Zone] ([Id], [MarketId], [Name], [CreateUtc], [ModifyUtc]) VALUES (3, 1, N'CAL_SE', N'2017-01-18 08:49:24', NULL)
+INSERT INTO [dbo].[Zone] ([Id], [MarketId], [Name], [CreateUtc], [ModifyUtc]) VALUES (4, 2, N'KY_SW', N'2017-01-18 08:51:48', NULL)
+INSERT INTO [dbo].[Zone] ([Id], [MarketId], [Name], [CreateUtc], [ModifyUtc]) VALUES (5, 2, N'KY_NW', N'2017-01-18 08:51:52', NULL)
+SET IDENTITY_INSERT [dbo].[Zone] OFF
+
+
