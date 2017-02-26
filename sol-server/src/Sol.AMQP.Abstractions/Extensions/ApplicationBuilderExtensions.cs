c70f0c062c274c58608e6a5f1cdd8e3683081c53
Zenon Trujillo <zenon.t@outlook.com>
2017-02-26 00:32:19 -0800

    building AMQP

---
+++ sol-server/src/Sol.AMQP.Abstractions/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using RabbitMQ.Client;
+
+namespace Sol.AMQP.Abstractions.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder SetUpAMQP(this IApplicationBuilder app, ConnectionFactory connectionFactory, Action<QueueManager> queueManagerAction)
+        {
+            var queueManager = new QueueManager(connectionFactory);
+            queueManagerAction(queueManager);
+            return app;
+        }
+
+        public static IApplicationBuilder DeclareQueue(this IApplicationBuilder app,
+            ConnectionFactory connectionFactory,
+            string queue,
+            bool durable = false,
+            bool exclusive = false,
+            bool autoDelete = false,
+            IDictionary<string, object> arguments = null)
+        {
+            using (IConnection connection = connectionFactory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue,
+                    durable,
+                    exclusive,
+                    autoDelete,
+                    arguments);
+            }
+
+            return app;
+        }
+
+        public static IApplicationBuilder DeclareQueue(this IApplicationBuilder app, 
+            string hostName, 
+            string queue, 
+            bool durable = false,
+            bool exclusive = false,
+            bool autoDelete = false,
+            IDictionary<string, object> arguments = null)
+        {
+            ConnectionFactory factory = new ConnectionFactory { HostName = hostName };
+            return app.DeclareQueue(factory, queue, durable, exclusive, autoDelete, arguments);
+        }
+
+        public static IApplicationBuilder ConsumeQueue(this IApplicationBuilder app,
+            ConnectionFactory connectionFactory,
+            string queue,
+            bool durable = false,
+            bool exclusive = false,
+            bool autoDelete = false,
+            IDictionary<string, object> arguments = null)
+        {
+            app = app.DeclareQueue(connectionFactory, queue, durable, exclusive, autoDelete, arguments);
+
+
+
+            return app;
+        }
+    }
+}
---
+++ sol-server/src/Sol.AMQP.Abstractions/QueueManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Sol.AMQP.Abstractions
+{
+    public class QueueManager : IDisposable
+    {
+        public QueueManager(IConnectionFactory connectionFactory)
+        {
+            Connection = connectionFactory.CreateConnection();
+            Channel = Connection.CreateModel();
+        }
+
+        public IConnection Connection { get; }
+
+        public IModel Channel { get; }
+
+        public void Declare(string queue,
+            bool durable = false,
+            bool exclusive = false,
+            bool autoDelete = false,
+            IDictionary<string, object> arguments = null)
+        {
+            Channel.QueueDeclare(queue,
+                durable,
+                exclusive,
+                autoDelete,
+                arguments);
+        }
+
+        public void Receive(string queue,
+            Action<object, BasicDeliverEventArgs> receiverAction,
+            bool noAck = true)
+        {
+            var consumer = new EventingBasicConsumer(Channel);
+            consumer.Received += (model, ea) =>
+            {
+                receiverAction(model, ea);
+            };
+            Channel.BasicConsume(queue,
+                                 noAck,
+                                 consumer);
+        }
+
+        public void Dispose()
+        {
+            Channel.Dispose();
+            Connection.Dispose();
+        }
+    }
+}
